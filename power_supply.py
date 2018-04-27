ef0628b97492042ef644d825b65ec7259886b369
Ville Ilvonen <vilvo@iki.fi>
2018-04-27 11:05:01 +0300

    set output voltage
    Signed-off-by: Ville Ilvonen <vilvo@iki.fi>

--- power_supply.py
+++ power_supply.py
@@ -8,18 +8,22 @@ class tenma():
         Tenma power supply - tested with Tenma 72-2535 over USB serial
         remote control protocol spec: https://goo.gl/YRNlPu '''
     def __init__(self):
-        self.s = serial.Serial('/dev/ttyACM3', baudrate=9600, timeout=1)
+        self.s = serial.Serial('/dev/ttyACM0', baudrate=9600, timeout=1)
     def off(self):
         self.s.write('OUT0')
     def on(self):
         self.s.write('OUT1')
+    def setvoltage(self, voltage):
+        cmd = "VSET1:%s" % (voltage)
+        for x in cmd:
+            self.s.write(x)
 
 if __name__ == '__main__':
     power_supply = tenma()
-    usage = 'usage: %s -p [ON|OFF]' % sys.argv[0]
+    usage = 'usage: %s -u [0-30] -p [ON|OFF]' % sys.argv[0]
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"hp:",["power="])
+        opts, args = getopt.getopt(sys.argv[1:],"hu:p:", ["help", "voltage=", "power="])
     except getopt.GetoptError:
         print usage
         sys.exit(2)
@@ -28,8 +32,19 @@ if __name__ == '__main__':
         if opt == '-h':
             print usage
             sys.exit()
+        elif opt == '-u':
+            try:
+                voltage = float(arg)
+            except:
+                print usage
+            if 0.0 <= voltage <= 30.0:
+                power_supply.setvoltage(str(voltage))
+            else:
+                print usage
         elif opt == '-p':
             if arg == 'ON':
                 power_supply.on()
             if arg == 'OFF':
                 power_supply.off()
+            else:
+                print usage
